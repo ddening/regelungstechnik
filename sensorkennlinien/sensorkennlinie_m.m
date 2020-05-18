7ffbcb6df8c625803b4ab6483cb0906ed7321d97
Dimitri <dimadening@web.de>
2020-05-18 16:19:57 +0200

    initial upload

---
+++ sensorkennlinien/sensorkennlinie_m.m
@@ -0,0 +1,9 @@
+% Arbeitsauftrag 3
+% Bestimmung der Sensorkennlinien
+% Erstellt am 18.05.2020
+% Author: Arnulf Mittelstädt, Dimitri Dening
+% Praktikumsgruppe: 4
+%% 
+close all % schließt alle offenen figures
+clear all % löscht alle Variablen im Worcspace
+clc % löscht das Command Window
