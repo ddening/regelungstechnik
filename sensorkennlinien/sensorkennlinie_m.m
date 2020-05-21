a27498c3cc573878ab145295c22f6b39a9937576
Dimitri <dimadening@web.de>
2020-05-21 17:29:41 +0200

    Sensorkennlinie ermittelt

--- sensorkennlinien/sensorkennlinie_m.m
+++ sensorkennlinien/sensorkennlinie_m.m
@@ -7,3 +7,30 @@
 close all % schließt alle offenen figures
 clear all % löscht alle Variablen im Worcspace
 clc % löscht das Command Window
+%% Starten der Simulations
+sim('Modell_der_Heizstrecke_s');
+%% Grafische Darstellung der Simulationsergebnisse
+figure, plot(tout, yout(:,1));
+xlabel('Zeit [s]');
+ylabel('Spannung [V]');
+
+figure, plot(tout, yout(:,2));
+xlabel('Zeit [s]');
+ylabel('Temperatur [°C]');
+
+%% Auswahl der Werte für die Bestimmung der Sensorkennlinie
+spannung = [yout(100,1), yout(300,1),yout(500,1),yout(700,1),yout(900,1)];
+temperatur = [yout(100,2), yout(300,2),yout(500,2),yout(700,2),yout(900,2)];
+plot(spannung, temperatur, '+');
+%% Bestimmung der Sensorkennlinie
+sensorkennlinie = polyfit(spannung, temperatur, 1);
+%% Erproben der Sensorkennlinie
+figure, plot(tout, yout(:,2));
+xlabel('Zeit [s]');
+ylabel('Temperatur [°C]');
+hold on;
+
+plot(tout, sensorkennlinie(1)*yout(:,1) + sensorkennlinie(2));
+xlabel('Zeit [s]');
+ylabel('Spannung umgewandelt in Temperatur [°C]');
+hold off;
