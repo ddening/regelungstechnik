cd39a3b600f0f19ae2c262c1fe977de05693b775
amittel <arnulf.mittelstaedt@gmail.com>
2020-05-22 09:01:41 +0200

    Zeige alle Graphen an

--- sensorkennlinien/sensorkennlinie_m.m
+++ sensorkennlinien/sensorkennlinie_m.m
@@ -10,17 +10,19 @@ clc % löscht das Command Window
 %% Starten der Simulations
 sim('Modell_der_Heizstrecke_s');
 %% Grafische Darstellung der Simulationsergebnisse
-figure, plot(tout, yout(:,1));
-xlabel('Zeit [s]');
-ylabel('Spannung [V]');
+subplot(2,1,1)
+plot(tout, yout(:,1))
+xlabel('Zeit [s]')
+ylabel('Spannung [V]')
 
-figure, plot(tout, yout(:,2));
-xlabel('Zeit [s]');
-ylabel('Temperatur [°C]');
+subplot(2,1,2)
+plot(tout, yout(:,2))
+xlabel('Zeit [s]')
+ylabel('Temperatur [°C]')
 %% Auswahl der Werte für die Bestimmung der Sensorkennlinie
 spannung = [yout(100,1), yout(300,1),yout(500,1),yout(700,1),yout(900,1)];
 temperatur = [yout(100,2), yout(300,2),yout(500,2),yout(700,2),yout(900,2)];
-plot(spannung, temperatur, '+');
+figure, plot(spannung, temperatur, '+');
 %% Bestimmung der Sensorkennlinie
 sensorkennlinie = polyfit(spannung, temperatur, 1);
 %% Erproben der Sensorkennlinie
